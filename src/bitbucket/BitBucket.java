ba2bb9d5af1b444b956057995dfc1b0d512ac325
EnesKaradza <>
2019-12-17 22:18:22 +0100

    1234 vjzeba
     - for petlja

---
+++ src/bitbucket/BitBucket.java
@@ -0,0 +1,14 @@
+
+package bitbucket;
+
+public class BitBucket {
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Pozdrav iz Jave");
+            
+        }
+    }
+    
+}
